f8c600f94fdd5e2a017c9026ce4952bbd5a6304b
José Fagner Lira Silva <ostiumdev@gmail.com>
2019-04-26 16:22:51 -0300

    First

---
+++ src/main/java/sintaxe/SintaxeJavaApplication.java
@@ -0,0 +1,13 @@
+package sintaxe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintaxeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintaxeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sintaxe/SintaxeJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package sintaxe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SintaxeJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
